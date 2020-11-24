e0012e6f58bd4b1cc5f8aafd28a69642ad85e61a
Tara Dia <taracdia@gmail.com>
2020-11-23 18:46:35 -0600

    cors fixed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const passport = require("passport")
+const config = require("./config");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,6 +31,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(passport.initialize());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -41,7 +45,6 @@ app.use("imageUpload", uploadRouter);
 
 const mongoose = require("mongoose");
 
-const config = require("./config");
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
--- routes/users.js
+++ routes/users.js
@@ -9,66 +9,69 @@ const router = express.Router();
 /* GET users listing. */
 
 router
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
 
-.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-  User.find()
-  .then(users => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json(users)
-  })
-  .catch(err => next(err));
-});
+  .get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    User.find()
+      .then(users => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(users)
+      })
+      .catch(err => next(err));
+  });
 
 router
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.post('/signup',cors.corsWithOptions,  (req, res) => {
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .post('/signup', cors.corsWithOptions, (req, res) => {
     User.register(
-      new User({username: req.body.username}),
+      new User({ username: req.body.username }),
       req.body.password,
       (err, user) => {
         if (err) {
           res.statusCode = 500;
           res.setHeader("Content-Type", "application/json");
-          res.json({err: err});
+          res.json({ err: err });
         } else {
-          if(req.body.firstname){
+          if (req.body.firstname) {
             user.firstname = req.body.firstname;
           }
-          if(req.body.lastname){
+          if (req.body.lastname) {
             user.lastname = req.body.lastname;
           }
           user.save(err => {
             if (err) {
               res.statusCode = 500;
               res.setHeader("Content-Type", "application/json");
-              res.json({err: err});
+              res.json({ err: err });
               return;
             }
             passport.authenticate("local")(req, res, () => {
               res.statusCode = 200;
               res.setHeader("Content-Type", "application/json");
-              res.json({success: true, status: "registration successful"});
+              res.json({ success: true, status: "registration successful" });
             })
           });
         }
       }
     )
-});
+  });
 
 router
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.post('/login',cors.corsWithOptions,  passport.authenticate("local"), (req, res) => {
-  const token = authenticate.getToken({_id: req.user._id})
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .post('/login', 
+  cors.corsWithOptions, 
+  passport.authenticate("local"), //line where it breaks
+  (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id })
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.json({success: true, token: token, status: "You are successfully logged in!"});
-});
+    res.json({ success: true, token: token, status: "You are successfully logged in!" });
+  });
 
 router
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.get('/logout',cors.corsWithOptions,  (req, res, next) => {
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get('/logout', cors.corsWithOptions, (req, res, next) => {
     if (req.session) {
       req.session.destroy();
       res.clearCookie('session-id');
@@ -78,7 +81,7 @@ router
       err.status = 403;
       return next(err);
     }
-});
+  });
 
 
 module.exports = router;
